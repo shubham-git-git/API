cbb02049351934e95bbb1fb65d7b60b240bd5712
Shubham gupta <shubham.gupta19701@gmail.com>
2025-06-14 23:04:03 +0530

    add new file id10

---
+++ DomainModel/SchoolEntities/TeacherEntities - Copy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.SchoolEntities
+{
+    public class TeacherEntities
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public DateTime DateOfJoining { get; set; }
+    }
+}
